4312d728692db9aae2ae3e33dcf93464969f1667
theprivatepark <anneypark@gmail.com>
2020-11-30 19:31:04 -0500

    got rid of attempted auth that failed terribly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,26 +2,5 @@ class ApplicationController < ActionController::API
 
   include ActionController::Helpers
   # skip_before_action :verify_authenticity_token # revents Rails from using its authenticity token security token that Rails generates from our session data and adds to the parameters sent from a Rails form to a controller action to prevent cross-site request forgery (CSRF) attacks. Since we are treating our back-end as an API, we should disable this so that we don’t receive ‘forbidden’ parameters that will prevent our controller actions from executing without errors
-  helper_method :login!, :logged_in?, :current_admin, :authorized_admin?, :logout! 
-  
-  def login!
-    session[:admin_id] = @admin.id
-  end
-
-  def logged_in?
-    !!session[:admin_id]
-  end
-
-  def current_admin
-    @current_admin ||= Admin.find(session[:admin_id]) if session[:admin_id]
-  end
-
-  def authorized_admin?
-     @admin == current_admin
-   end
-
-  def logout!
-     session.clear
-   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,7 @@ Rails.application.routes.draw do
   resources :admins
   resources :images
 
-  # post '/login', to: 'sessions#create'
-  # delete '/logout', to: 'sessions#destroy'
-  # get '/logged_in', to: 'sessions#is_logged_in?'
   post "/login", to: "admins#login", as: "login"
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
